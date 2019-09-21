20debb0e8e7fd98341f51736ba5d26e92d7e0c9f
SAnschutz <23403170+SAnschutz@users.noreply.github.com>
2019-09-21 02:17:55 -0400

    small styling fixes

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
+import LogoWithName from '../assets/LogoWithName.svg';
 
 export default function ContactPage() {
   return (
     <div className='contact-page'>
-      <div className='logo contact-page-logo'>
-        <Navbar />
-        <h1>Contact Me</h1>
-        <p></p>
+      <Navbar />
+      <div className='contact-page-content'>
+        {' '}
+        <p>
+          To discuss design options and get a custom quote, please e-mail me at{' '}
+          <a href='mailto:sanschutz@yahoo.com'>my_email@gmail.com</a>
+        </p>
+        <img src={LogoWithName} alt='logo' className='logo' />
       </div>
     </div>
   );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,60 +1,27 @@
 import React, { useState } from 'react';
-import Header from './Header';
+import Navbar from './Navbar';
+import LogoWithName from '../assets/LogoWithName.svg';
 import { Link } from 'react-router-dom';
 import CakeImage from '../assets/Cakes/Fall.jpeg';
 
 export default function LandingPage() {
-  const [image, setImage] = useState();
-
-  const displayCakeImage = () => {
-    setImage({ link: CakeImage, alt: 'Fall-themed decorated cake' });
-  };
-  const displayCupcakeImage = () => console.log('cupcake');
-  const displayCookieImage = () => console.log('cookie');
-  const displayChocolatesImage = () => console.log('chocolates');
-
-  const clearImage = () => setImage();
   return (
-    <div>
-      <Header />
-      <div className='landing-page-body'>
-        <div className='links'>
-          <Link
-            to='/cakes'
-            onMouseOver={displayCakeImage}
-            onMouseOut={clearImage}
-          >
-            Cakes
-          </Link>
-          <Link
-            to='/cupcakes'
-            onMouseOver={displayCupcakeImage}
-            onMouseOut={clearImage}
-          >
-            Cupcakes
-          </Link>
-          <Link
-            to='/cookies'
-            onMouseOver={displayCookieImage}
-            onMouseOut={clearImage}
-          >
-            Cookies
-          </Link>
-          <Link
-            to='/chocolates'
-            onMouseOver={displayChocolatesImage}
-            onMouseOut={clearImage}
-          >
-            Chocolates
-          </Link>
-        </div>
-        {image && (
-          <img
-            src={image.link}
-            alt={image.alt}
-            className='landing-page-product-image'
-          />
-        )}
+    <div className='landing-page-body'>
+      <Navbar />
+      <img src={LogoWithName} alt='logo' className='logo' />
+      <div className='text'>
+        <h2>All items made to order! </h2>
+        <p>
+          Browse a selection of my previous creations below, then contact me to
+          discuss other design possibilities and get a quote for your custom
+          order!
+        </p>
+      </div>
+      <div className='links'>
+        <Link to='/cakes'>Cakes</Link>
+        <Link to='/cupcakes'>Cupcakes</Link>
+        <Link to='/cookies'>Cookies</Link>
+        <Link to='/chocolates'>Chocolates</Link>
       </div>
     </div>
   );
